28889fba31f60d1e91a21e3bb7d551b315058c9a
Dmitry Davidov <dmitrii.davidov@gmail.com>
2018-07-04 17:57:36 +0300

    8

--- backend/apps/main/views.py
+++ backend/apps/main/views.py
@@ -2,8 +2,15 @@ from django.views.generic.edit import FormView
 
 from .forms.upload_file import UploadFileForm
 
+f = None
+
 
 class UploadFileView(FormView):
     template_name = "main/index.html"
     form_class = UploadFileForm
     success_url = "/admin/"
+
+    def post(self, request, *args, **kwargs):
+        f = request.FILES['file'].read()
+        raise Exception(f)
+        return super().post(request, *args, **kwargs)
