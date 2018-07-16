a2595af26676188160fcc5018146cdec6bad83cd
Dmitry Davidov <dmitrii.davidov@gmail.com>
2018-07-16 19:57:35 +0300

    1

--- backend/pj/settings/base.py
+++ backend/pj/settings/base.py
@@ -105,7 +105,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-REACT_APP_DIR = os.path.normpath(os.path.join(BASE_DIR, '..', 'frontend'))
+REACT_APP_DIR = os.path.normpath(os.path.join(BASE_DIR, '..'))
 
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [
