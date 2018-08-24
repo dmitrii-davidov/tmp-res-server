65fb843a32aa2b78e076f4ae12bc30e82d0e4cdf
Dmitry Davidov <dmitrii.davidov@gmail.com>
2018-08-24 18:41:37 +0300

    1

--- backend/pj/settings/master.py
+++ backend/pj/settings/master.py
@@ -1,11 +1,6 @@
-import dj_database_url
-
 from .base import *  # noqa:F401,F403 # pylint:disable=W0401,W0614
 
 DEBUG = True
-
-db_from_env = dj_database_url.config(conn_max_age=500, ssl_require=True)
-DATABASES['default'] = db_from_env
 
 try:
     from .local import *  # noqa:F401,F403 # pylint:disable=W0401,W0614
